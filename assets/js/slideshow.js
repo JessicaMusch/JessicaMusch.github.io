7af1fcdb2fa94d75c53c645d97b72884f7ec6ded
JessicaMusch <jes.musch@gmail.com>
2025-12-17 12:24:12 +0100

    Portfolio 1.2
    Changed the Design and added more projects

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,32 +1,17 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// control of scrolling through slides
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// first image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+let slideIndex = 0;
+showSlides();
 
 // main function for slides
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("dot");
-    
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
+    let slides = document.getElementsByClassName("slide");
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
+    slideIndex++;
+    if(slideIndex > slides.length){slideIndex = 1};
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides,6000);
 }
